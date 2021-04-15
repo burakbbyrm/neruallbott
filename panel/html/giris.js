aba410b002fe65a5d91879d39ae280b11b8dbd58
burakbbyrm <46279893+burakbbyrm@users.noreply.github.com>
2021-04-15 15:31:48 +0300

    Add files via upload

---
+++ panel/html/giris.js
@@ -0,0 +1,7 @@
+(function() {
+
+    document.getElementById("target").onclick = function() {
+      var wnd = window.open('https://discordapp.com/oauth2/authorize?client_id=437439973751521280&permissions=314440&scope=bot', 'JackBot Invite', 'width=470,height=640');
+    };
+  
+  })();
